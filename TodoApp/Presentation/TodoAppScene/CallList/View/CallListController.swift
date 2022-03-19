3465debeb90ce006e68082cc65c70280886c8b84
Khoa Pham <khoapv0903@gmail.com>
2022-03-19 21:44:52 +0700

    Implement Todo List screen

---
+++ TodoApp/Presentation/TodoAppScene/CallList/View/CallListController.swift
@@ -0,0 +1,19 @@
+//
+//  CallListController.swift
+//  TodoApp
+//
+//  Created by Khoa Pham on 19/03/2022.
+//
+
+import UIKit
+
+class CallListController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Call List"
+        view.backgroundColor = .white
+    }
+}
---
+++ TodoApp/Presentation/TodoAppScene/TodoList/View/TodoListController.swift
@@ -0,0 +1,77 @@
+//
+//  TodoListController.swift
+//  TodoApp
+//
+//  Created by Khoa Pham on 19/03/2022.
+//
+
+import UIKit
+
+class TodoListController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private lazy var callListButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .black
+        button.setTitle("Call List", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.addTarget(self, action: #selector(didTapCallList), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var buyListButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .black
+        button.setTitle("Buy List", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.addTarget(self, action: #selector(didTapBuyList), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var sellListButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .black
+        button.setTitle("Sell List", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.addTarget(self, action: #selector(didTapSellList), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationItem.title = "Todo List"
+        
+        let stack = UIStackView(arrangedSubviews: [callListButton, buyListButton, sellListButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+        
+        view.addSubview(stack)
+        stack.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+    }
+    
+    // MARK: - Actions
+    
+    @objc func didTapCallList() {
+        print("didTapCallList")
+        navigationController?.pushViewController(CallListController(), animated: true)
+    }
+    
+    @objc func didTapBuyList() {
+        print("didTapBuyList")
+    }
+    
+    @objc func didTapSellList() {
+        print("didTapSellList")
+    }
+}
